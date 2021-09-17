06401d9b55d8558e495c2348edad7b0c56e17634
mariusraht1 <mariusraht+1@gmail.com>
2021-09-17 16:27:48 +0200

    Remove obsolete source code

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-const express = require("express");
-const app = express();
-const port = 3000;
 const mongoose = require("mongoose");
 
 mongoose.connect("mongodb://localhost:27017", { useNewUrlParser: true });
